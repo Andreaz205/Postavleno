6618d9a68eddb9a07823fd1f68f06e22ead3633d
Андрей <andrei.baz1337@gmail.com>
2024-03-06 13:11:22 +0500

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import {defineConfig} from "vite";
+import {resolve} from "path";
 
 export default defineConfig({
     build: {
         rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                thanks: resolve(__dirname, 'thanks.html'),
+                "404": resolve(__dirname, '404.html'),
+            },
             output: {
                 entryFileNames: `assets/[name].js`,
                 chunkFileNames: `assets/[name].js`,
